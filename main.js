dc9ea1d8b2f68b5f2531b8c08f7827acbf06a188
fantoro <fantoro@outlook.com>
2020-01-01 20:48:26 +0100

    Made the script remove the "Who to follow" blocks that show up on profiles

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function removeAllElements(col) {
+	if(col.length < 1) return;
 	for (i in col){
 		col[i].parentNode.removeChild(col[i]);
 	}
@@ -6,4 +7,17 @@ function removeAllElements(col) {
 
 setInterval(() => {
 	removeAllElements(document.getElementsByClassName("css-1dbjc4n r-1uaug3w r-1uhd6vh r-t23y2h r-1phboty r-rs99b7 r-ku1wi2 r-1bro5k0 r-1udh08x"));
+},100);
+
+setInterval(() => {
+	var elements = document.getElementsByClassName("css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0");
+	for (i in elements){
+		if(elements[i].innerText == "Who to follow"){
+			var block = elements[i].parentNode.parentNode.parentNode.parentNode.parentNode;
+			block.parentNode.removeChild(block.nextElementSibling);
+			block.parentNode.removeChild(block.nextElementSibling);
+			block.parentNode.removeChild(block.nextElementSibling);
+			block.parentNode.removeChild(block);
+		}
+	}
 },100);
